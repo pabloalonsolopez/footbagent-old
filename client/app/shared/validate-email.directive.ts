74eee549e4e5785c2942d6865745f27228beac69
Pablo Alonso López <pabloalonsolopez@gmail.com>
2016-07-14 08:52:40 +0200

    More stuff

--- client/app/shared/validate-email.directive.ts
+++ client/app/shared/validate-email.directive.ts
@@ -1,16 +1,16 @@
 import { Directive } from '@angular/core'
-import { NG_VALIDATORS, FormControl } from '@angular/forms'
+import { NG_VALIDATORS, Validator, FormControl } from '@angular/forms'
 
 @Directive({
   selector: '[validateEmail][ngModel]',
-  providers: [
-  	{
-  		provide: NG_VALIDATORS,
-  		useValue: function(formControl: FormControl) {
-  			return { validateEmail: { valid: false } }
-  		},
-  		multi: true
-  	}]
+  providers: [{provide: NG_VALIDATORS, useExisting: ValidateEmailDirective, multi: true}]
 })
 
-export class ValidateEmailDirective { }
+export class ValidateEmailDirective implements Validator {
+  
+  validate(formControl: FormControl): {[key: string]: any} {
+    let EMAIL_REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return EMAIL_REGEXP.test(formControl.value) ? null : { "validateEmail": true }
+  }
+
+}
